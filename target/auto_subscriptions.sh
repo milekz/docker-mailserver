b36e8859c8a0d1ffbcdb7ba82ac0652fc7097395
Miłosz Szewczak <milek@milek.eu>
2018-04-02 13:05:53 +0100

    Update auto_subscriptions.sh

--- target/auto_subscriptions.sh
+++ target/auto_subscriptions.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #auto subscriptions for dovecot run in dir contain mailboxes
-for filename in */; do
-    echo doing - $filename
-    cd $filename
-    ls -1 -d -A .*| tail -n +3 | cut -c 2- > subscriptions
-    chown 5000:5000 subscriptions
+for dname in */; do
+    echo doing domain $dname
+    cd $dname
+        for filename in */; do
+	        echo doing account - $filename
+	        cd $filename
+	        ls -1 -d -A .*| tail -n +3 | cut -c 2- > subscriptions
+	        chown 5000:5000 subscriptions
+	        cd ..
+	      done
     cd ..
 done
